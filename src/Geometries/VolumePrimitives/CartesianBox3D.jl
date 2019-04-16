137cce4c175780af2eeb59cd1c5c5f06de993129
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-16 22:58:53 +0000

    Snapshot of branch dev taken on 2019-04-16T18:58:52.657-04:00 from "https://github.com/UnofficialJuliaMirror/SolidStateDetectors.jl-71e43887-2bd9-5f77-aebd-47f656f0a3f0"

--- src/Geometries/VolumePrimitives/CartesianBox3D.jl
+++ src/Geometries/VolumePrimitives/CartesianBox3D.jl
@@ -15,7 +15,7 @@ end
 
 @inline in(pt::CylindricalPoint, g::CartesianBox3D)::Bool = in(CartesianPoint(pt), g)
 
-function CartesianBox3D{T}(dict::Dict{Any, Any}, inputunit::Unitful.Units)::CartesianBox3D{T} where {T <: AbstractFloat}
+function CartesianBox3D{T}(dict::Dict{Any, Any}, inputunit::Unitful.Units)::CartesianBox3D{T} where {T <: SSDFloat}
     return CartesianBox3D{T}(
         (geom_round(ustrip(uconvert(u"m", T(dict["xStart"]) * inputunit ))), geom_round(ustrip(uconvert(u"m", T(dict["xStop"]) * inputunit)))),
         (geom_round(ustrip(uconvert(u"m", T(dict["yStart"]) * inputunit ))), geom_round(ustrip(uconvert(u"m", T(dict["yStop"]) * inputunit)))),
@@ -26,28 +26,67 @@ function Geometry(T::DataType, t::Val{:CartesianBox3D}, dict::Dict{Any, Any}, in
     return CartesianBox3D{T}(dict, inputunit)
 end
 
-function get_important_points(g::CartesianBox3D{T})::NTuple{3, Vector{T}} where {T <: AbstractFloat}
+function get_important_points(g::CartesianBox3D{T})::NTuple{3, Vector{T}} where {T <: SSDFloat}
     v1::Vector{T} = T[g.x[1], g.x[2]] #[g.x[1], g.x[2]]
     v2::Vector{T} = T[g.y[1], g.y[2]]
     v3::Vector{T} = T[g.z[1], g.z[2]]
     return v1, v2, v3
 end
 
-function get_important_points(g::CartesianBox3D{T}, ::Val{:r})::Vector{T} where {T <: AbstractFloat}
+function get_important_points(g::CartesianBox3D{T}, ::Val{:r})::Vector{T} where {T <: SSDFloat}
     @warn "Not yet implemented"
     return T[]
 end
-function get_important_points(g::CartesianBox3D{T}, ::Val{:φ})::Vector{T} where {T <: AbstractFloat}
+function get_important_points(g::CartesianBox3D{T}, ::Val{:φ})::Vector{T} where {T <: SSDFloat}
     @warn "Not yet implemented"
     return T[]
 end
-function get_important_points(g::CartesianBox3D{T}, ::Val{:z})::Vector{T} where {T <: AbstractFloat}
+function get_important_points(g::CartesianBox3D{T}, ::Val{:z})::Vector{T} where {T <: SSDFloat}
     return T[g.z[1], g.z[2]]
 end
-function get_important_points(g::CartesianBox3D{T}, ::Val{:x})::Vector{T} where {T <: AbstractFloat}
+function get_important_points(g::CartesianBox3D{T}, ::Val{:x})::Vector{T} where {T <: SSDFloat}
     return T[g.x[1], g.x[2]]
 end
-function get_important_points(g::CartesianBox3D{T}, ::Val{:y})::Vector{T} where {T <: AbstractFloat}
+function get_important_points(g::CartesianBox3D{T}, ::Val{:y})::Vector{T} where {T <: SSDFloat}
     return T[g.y[1], g.y[2]]
 end
 
+
+function vertices(cb::CartesianBox3D{T})::Vector{CartesianPoint{T}} where {T <: SSDFloat}
+    v::Vector{CartesianPoint{T}} = CartesianPoint{T}[
+        CartesianPoint{T}(cb.x[1], cb.y[1], cb.z[1]),
+        CartesianPoint{T}(cb.x[2], cb.y[1], cb.z[1]),
+        CartesianPoint{T}(cb.x[2], cb.y[2], cb.z[1]),
+        CartesianPoint{T}(cb.x[1], cb.y[2], cb.z[1]),
+        CartesianPoint{T}(cb.x[1], cb.y[1], cb.z[2]),
+        CartesianPoint{T}(cb.x[2], cb.y[1], cb.z[2]),
+        CartesianPoint{T}(cb.x[2], cb.y[2], cb.z[2]),
+        CartesianPoint{T}(cb.x[1], cb.y[2], cb.z[2]),
+    ]    
+    return v
+end
+
+function LineSegments(cb::CartesianBox3D{T})::Vector{LineSegment{T, 3, :Cartesian}} where {T}
+    v::Vector{CartesianPoint{T}} = vertices(cb)
+    return LineSegment{T, 3, :Cartesian}[
+        LineSegment(v[1], v[2]),
+        LineSegment(v[2], v[3]),
+        LineSegment(v[3], v[4]),
+        LineSegment(v[4], v[1]),
+        LineSegment(v[1], v[5]),
+        LineSegment(v[2], v[6]),
+        LineSegment(v[3], v[7]),
+        LineSegment(v[4], v[8]),
+        LineSegment(v[5], v[6]),
+        LineSegment(v[6], v[7]),
+        LineSegment(v[7], v[8]),
+        LineSegment(v[8], v[5])
+    ]
+end
+
+@recipe function f(cb::CartesianBox3D{T}) where {T <: SSDFloat}
+    ls = LineSegments(cb)
+    @series begin
+        ls
+    end
+end
